265601d68d3565b07e99b26faf6db67760ee3f18
innotuyi <innotuyi67@gmail.com>
2022-05-05 16:38:56 +0200

    implementing Product relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    const AVAILABLE_PRODUCT= 'available';
+    const UNAVAILABLE_PRODUCT= 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+
+    public function isAvailable(){
+        return $this->status==Product::AVAILABLE_PRODUCT;
+    }
+
+
+
+     public function seller(){
+         return $this->hasOne(Seller::class);
+     }
+
+     public function transacton(){
+         return $this->hasMany(Transaction::class)
+     }
+    public function categories(){
+
+        return $this->hasMany(Category::class);
+    }
 }
