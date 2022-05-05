61de5d19a3dfd49ea2478b405911c28364085707
innotuyi <innotuyi67@gmail.com>
2022-05-05 16:39:30 +0200

    Implementing seller relationship

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -4,5 +4,18 @@ namespace App\Models;
 
 class Seller extends User
 {
-  
+    const AVAILABLE_PRODUCT= 'available';
+    const UNAVAILABLE_PRODUCT= 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
+
+
+
 }
