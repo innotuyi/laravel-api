541a0856cfa4cc2e51c66b13b7a327a9673d03da
innotuyi <innotuyi67@gmail.com>
2022-05-05 16:43:18 +0200

    updating user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,20 +7,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const VERIFIED_USER='1';
+    CONST UNVERIFIED_USER='0';
+
+    CONST ADMIN_USER = 'true';
+    CONST REGULAR_USER = 'false';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
+     * 
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'verified',
+        'verification_token',
+        'admin'
     ];
 
     /**
@@ -31,6 +42,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'verification_token'
     ];
 
     /**
@@ -40,5 +52,19 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+    ]; 
+
+    public function isVerified(){
+        return $this->verified=User::VERIFIED_USER;
+    }
+    public function isAdmin(){
+        return $this->admin == User::ADMIN_USER;
+            
+        
+    }
+
+    public static function generateVerificationCode(){
+        return Str::random(40);
+        
+    }
 }
