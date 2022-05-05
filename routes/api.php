a5dd65da71b0ab84550d4edf48ab03fb1022dba3
innotuyi <innotuyi67@gmail.com>
2022-05-05 15:11:39 +0200

    creating main routes

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,12 @@
 <?php
 
+use App\Http\Controllers\BuyerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::resource('buyers', BuyerController::class, ['only'=>['index', 'show']]);
+Route::resource('categories', BuyerController::class, ['except'=>['create', 'edit']]);
+Route::resource('products', BuyerController::class, ['only'=>['index', 'show']]);
+Route::resource('sellers', BuyerController::class, ['only'=>['index', 'show']]);
+Route::resource('transactions', BuyerController::class, ['only'=>['index', 'show']]);
+Route::resource('users', BuyerController::class, ['except'=>['create', 'edit']]);
