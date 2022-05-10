e80228e15d807d326b6cc220fa88c1ffe9a8b503
innotuyi <innotuyi67@gmail.com>
2022-05-10 17:22:51 +0200

    product seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Product::truncate();
+        $productsQuantity = 1000;
+
+        Product::factory($productsQuantity)->create()->each(
+            function($product){
+                $categories =Category::all()->random(mt_rand(1, 15))->pluck('id');
+                $product->categories->attach($categories);
+            }
+        );
+        
+    }
+}
