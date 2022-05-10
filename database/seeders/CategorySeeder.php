4938770bfaed04c0318876e2ffa2c0fce5098edc
innotuyi <innotuyi67@gmail.com>
2022-05-10 17:22:15 +0200

    Adding category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Category::truncate();
+        $categoriesQuantity = 30;
+        Category::factory($categoriesQuantity)->create();
+        
+    }
+}
