0c74373d171dc0796be421df1da5e42376888f08
innotuyi <innotuyi67@gmail.com>
2022-06-03 15:46:15 +0200

    Adding traits to buyer controller

--- app/Http/Controllers/BuyerController.php
+++ app/Http/Controllers/BuyerController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Buyer;
+use App\Traits\Responser;
 use Illuminate\Http\Request;
-
 class BuyerController extends ApiController
 {
+    use Responser;
     public function index()
     {
         $buyers = Buyer::has('transactions')->get();
